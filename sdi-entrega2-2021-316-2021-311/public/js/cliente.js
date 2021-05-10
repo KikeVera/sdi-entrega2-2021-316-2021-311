541fd6ae4a97e7fd04c6c578004ab9612a30ba03
UO246711 <UO246711@uniovi.es>
2021-05-10 16:28:33 +0200

    API y cliente JQuery con código comentado

--- sdi-entrega2-2021-316-2021-311/public/js/cliente.js
+++ sdi-entrega2-2021-316-2021-311/public/js/cliente.js
@@ -1,13 +1,18 @@
+
+//Se inicializan las variables, el token, la url y dos id que se usarán en la conversación de una oferta
 let token;
 let idOfertaSeleccionada;
 let idConversacion;
 let URLbase = "https://localhost:8081/api";
 
 
-
+//Se obtiene el tooken de las cookies
 token = Cookies.get('token');
+//Se obtiene cual fue la ultima página a la que se accedió
 let url = new URL(window.location.href);
 let w = url.searchParams.get("w");
+
+//Si se tiene almacenada alguna se accede a ella
 if ( w === "login"){
     $( "#contenedor-principal" ).load("widget-login.html");
 }
@@ -22,6 +27,7 @@ else if ( w === "conversationList"){
     $( "#contenedor-principal" ).load("widget-conversationList.html");
 }
 
+//Si no se tiene ninguna se accede a ofertas si se está logeado y al login si no se está
 else{
     if ( token != null ) {
         $( "#contenedor-principal" ).load("widget-ofertas.html");
@@ -35,24 +41,31 @@ else{
 
 updateNav();
 
+//Función para actualizar la barra de navegación
 function updateNav(){
-    $("#barra-menu").empty();
-    $("#barra-menu-derecha").empty();
+    var izq= $("#barra-menu");
+    var der= $("#barra-menu-derecha");
+    //Se limpian el html de las dos secciones de la barra
 
+   izq.empty();
+   der.empty();
+
+    //Si estamos logeados se muestran los lugares donde podemos acceder deslogeados
     if ( token != null ) {
-        $("#barra-menu").append(
+        izq.append(
             " <li id='mOfertas'><a onClick=widgetOfertas()> Ofertas</li>"
         );
-        $("#barra-menu").append(
+        izq.append(
             " <li id='mConversaciones'><a onClick=widgetConversaciones()> Mis conversaciones</li>"
         );
-        $("#barra-menu-derecha").append(
+        der.append(
             "<li id='mdesconectar'><a onclick=desconectar()><span class=\"glyphicon glyphicon-log-out\"></span> Desconectar</a></li>"
 
         );
     }
+    //Si no lo estamos se muestra el login
     else{
-        $("#barra-menu-derecha").append(
+        der.append(
             "<li id='mLogin'><a onclick=widgetLogin()><span class='glyphicon glyphicon-log-in'></span> Identifícate</a></li>"
 
         );
